29f3cdbce57880d426fb172f32a281a10dced888
masataka-maeda19 <108006776+masataka-maeda19@users.noreply.github.com>
2023-04-05 18:00:27 +0900

    Delete 03_スケーラブルWeb directory

--- 03_スケーラブルWeb/terraform/generated/aws/iam/iam_user_policy_attachment.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user_policy_attachment" "tfer--test2_AmazonManagedBlockchainFullAccess" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonManagedBlockchainFullAccess"
-  user       = "test2"
-}
-
-resource "aws_iam_user_policy_attachment" "tfer--test3_AdministratorAccess" {
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  user       = "test3"
-}
--- 03_スケーラブルWeb/terraform/generated/aws/rds/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_db_instance_tfer--database-1_id" {
-  value = "${aws_db_instance.tfer--database-1.id}"
-}
-
-output "aws_db_subnet_group_tfer--db-0020-subnet-0020-user1_id" {
-  value = "${aws_db_subnet_group.tfer--db-0020-subnet-0020-user1.id}"
-}
